e547bdfe1e7d4863fe804b9a8dfbca47e2bbd199
Eric Yue <yuekewin@gmail.com>
2026-02-23 13:32:31 +0800

    feat: update redis structure

--- go.mod
+++ go.mod
@@ -4,19 +4,30 @@ go 1.24.12
 
 require (
 	github.com/alicebob/miniredis/v2 v2.35.0
+	github.com/chaos-io/chaos v0.1.4-0.20260103071414-3f0d10bc538b
 	github.com/pkg/errors v0.9.1
 	github.com/redis/go-redis/v9 v9.17.2
 	github.com/stretchr/testify v1.11.1
 )
 
 require (
+	dario.cat/mergo v1.0.1 // indirect
+	github.com/bitly/go-simplejson v0.5.1 // indirect
 	github.com/cespare/xxhash/v2 v2.3.0 // indirect
+	github.com/chaos-io/core/go v0.0.0-20251009072706-721cc34b146d // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
-	github.com/kr/pretty v0.3.1 // indirect
+	github.com/fatih/structs v1.1.0 // indirect
+	github.com/fsnotify/fsnotify v1.6.0 // indirect
+	github.com/json-iterator/go v1.1.12 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
-	github.com/rogpeppe/go-internal v1.11.0 // indirect
 	github.com/yuin/gopher-lua v1.1.1 // indirect
-	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
+	go.uber.org/multierr v1.11.0 // indirect
+	go.uber.org/zap v1.27.0 // indirect
+	golang.org/x/sys v0.38.0 // indirect
+	google.golang.org/protobuf v1.36.6 // indirect
+	gopkg.in/natefinch/lumberjack.v2 v2.2.1 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- redis.go
+++ redis.go
@@ -1,149 +1,83 @@
 package redis
 
 import (
-	"context"
+	"sync"
 	"time"
 
-	"github.com/redis/go-redis/v9"
+	goredis "github.com/redis/go-redis/v9"
 )
 
-// NewClient creates a new Redis client
-// TODO: define a Config type with json/yaml tags.
-func NewClient(opts *redis.Options) (Cmdable, error) {
-	cli := redis.NewClient(opts)
-	return &provider{cli: cli}, nil
+var (
+	redisClient     goredis.UniversalClient
+	redisClientOnce sync.Once
+)
+
+type Redis struct {
+	Client goredis.UniversalClient
 }
 
-func Unwrap(cli Cmdable) (*redis.Client, bool) {
-	if p, ok := cli.(interface {
-		Raw() *redis.Client
-	}); ok {
-		return p.Raw(), true
+func GetRedis() goredis.UniversalClient {
+	redisClientOnce.Do(func() {
+		cfg := NewConfig()
+		redisClient = New(cfg).Client
+	})
+	return redisClient
+}
+
+func New(cfg *Config) *Redis {
+	if cfg == nil {
+		cfg = NewConfig()
 	}
-	return nil, false
-}
 
-type provider struct {
-	cli *redis.Client
-}
+	if len(cfg.Connections) == 0 {
+		cfg.Connections = []string{":6379"}
+	}
 
-func (p *provider) Exists(ctx context.Context, keys ...string) *redis.IntCmd {
-	return p.cli.Exists(ctx, keys...)
-}
+	if cfg.MinIdleConns == 0 {
+		if cfg.MaxIdleConns > 0 {
+			cfg.MinIdleConns = cfg.MaxIdleConns
+		} else {
+			cfg.MinIdleConns = 100
+		}
+	}
 
-var _ Cmdable = (*provider)(nil)
+	if cfg.PoolSize == 0 {
+		cfg.PoolSize = 300
+	}
 
-// Raw returns the underlying Redis client.
-// Use with caution.
-func (p *provider) Raw() *redis.Client {
-	return p.cli
-}
+	if cfg.ReadTimeout == 0 {
+		cfg.ReadTimeout = time.Second
+	}
+	if cfg.WriteTimeout == 0 {
+		cfg.WriteTimeout = cfg.ReadTimeout
+	}
 
-func (p *provider) Decr(ctx context.Context, key string) *redis.IntCmd {
-	return p.cli.Decr(ctx, key)
-}
+	if len(cfg.Connections) == 1 {
+		option := &goredis.Options{
+			Addr:            cfg.Connections[0],
+			Password:        cfg.Password,
+			DB:              cfg.DB,
+			MinIdleConns:    cfg.MinIdleConns,
+			PoolSize:        cfg.PoolSize,
+			ReadTimeout:     cfg.ReadTimeout,
+			WriteTimeout:    cfg.WriteTimeout,
+			MaxRetries:      cfg.MaxRetries,
+			MaxRetryBackoff: cfg.MaxRetryBackoff,
+			MinRetryBackoff: cfg.MinRetryBackoff,
+		}
+		return &Redis{Client: goredis.NewClient(option)}
+	}
 
-func (p *provider) DecrBy(ctx context.Context, key string, decrement int64) *redis.IntCmd {
-	return p.cli.DecrBy(ctx, key, decrement)
-}
-
-func (p *provider) Get(ctx context.Context, key string) *redis.StringCmd {
-	return p.cli.Get(ctx, key)
-}
-
-func (p *provider) Incr(ctx context.Context, key string) *redis.IntCmd {
-	return p.cli.Incr(ctx, key)
-}
-
-func (p *provider) IncrBy(ctx context.Context, key string, increment int64) *redis.IntCmd {
-	return p.cli.IncrBy(ctx, key, increment)
-}
-
-func (p *provider) MGet(ctx context.Context, keys ...string) *redis.SliceCmd {
-	return p.cli.MGet(ctx, keys...)
-}
-
-func (p *provider) MSet(ctx context.Context, values ...any) *redis.StatusCmd {
-	return p.cli.MSet(ctx, values...)
-}
-
-func (p *provider) MSetNX(ctx context.Context, values ...any) *redis.BoolCmd {
-	return p.cli.MSetNX(ctx, values...)
-}
-
-func (p *provider) Set(ctx context.Context, key string, value any, expiration time.Duration) *redis.StatusCmd {
-	return p.cli.Set(ctx, key, value, expiration)
-}
-
-func (p *provider) SetNX(ctx context.Context, key string, value any, expiration time.Duration) *redis.BoolCmd {
-	return p.cli.SetNX(ctx, key, value, expiration)
-}
-
-func (p *provider) HDel(ctx context.Context, key string, fields ...string) *redis.IntCmd {
-	return p.cli.HDel(ctx, key, fields...)
-}
-
-func (p *provider) HExists(ctx context.Context, key, field string) *redis.BoolCmd {
-	return p.cli.HExists(ctx, key, field)
-}
-
-func (p *provider) HGet(ctx context.Context, key, field string) *redis.StringCmd {
-	return p.cli.HGet(ctx, key, field)
-}
-
-func (p *provider) HGetAll(ctx context.Context, key string) *redis.MapStringStringCmd {
-	return p.cli.HGetAll(ctx, key)
-}
-
-func (p *provider) HIncrBy(ctx context.Context, key, field string, increment int64) *redis.IntCmd {
-	return p.cli.HIncrBy(ctx, key, field, increment)
-}
-
-func (p *provider) HKeys(ctx context.Context, key string) *redis.StringSliceCmd {
-	return p.cli.HKeys(ctx, key)
-}
-
-func (p *provider) HLen(ctx context.Context, key string) *redis.IntCmd {
-	return p.cli.HLen(ctx, key)
-}
-
-func (p *provider) HMGet(ctx context.Context, key string, fields ...string) *redis.SliceCmd {
-	return p.cli.HMGet(ctx, key, fields...)
-}
-
-func (p *provider) HSet(ctx context.Context, key string, values ...any) *redis.IntCmd {
-	return p.cli.HSet(ctx, key, values...)
-}
-
-func (p *provider) HSetNX(ctx context.Context, key, field string, value any) *redis.BoolCmd {
-	return p.cli.HSetNX(ctx, key, field, value)
-}
-
-func (p *provider) ZAdd(ctx context.Context, key string, members ...redis.Z) *redis.IntCmd {
-	return p.cli.ZAdd(ctx, key, members...)
-}
-
-func (p *provider) ZAddNX(ctx context.Context, key string, members ...redis.Z) *redis.IntCmd {
-	return p.cli.ZAddNX(ctx, key, members...)
-}
-
-func (p *provider) ZRange(ctx context.Context, key string, start, stop int64) *redis.StringSliceCmd {
-	return p.cli.ZRange(ctx, key, start, stop)
-}
-
-func (p *provider) Del(ctx context.Context, keys ...string) *redis.IntCmd {
-	return p.cli.Del(ctx, keys...)
-}
-
-func (p *provider) Eval(ctx context.Context, script string, keys []string, args ...any) *redis.Cmd {
-	return p.cli.Eval(ctx, script, keys, args...)
-}
-
-func (p *provider) Expire(ctx context.Context, key string, expiration time.Duration) *redis.BoolCmd {
-	return p.cli.Expire(ctx, key, expiration)
-}
-
-func (p *provider) Pipeline() Pipeliner {
-	pipe := p.cli.Pipeline()
-	return pipe
+	clusterOption := &goredis.ClusterOptions{
+		Addrs:           cfg.Connections,
+		Password:        cfg.Password,
+		MinIdleConns:    cfg.MinIdleConns,
+		PoolSize:        cfg.PoolSize,
+		ReadTimeout:     cfg.ReadTimeout,
+		WriteTimeout:    cfg.WriteTimeout,
+		MaxRetries:      cfg.MaxRetries,
+		MaxRetryBackoff: cfg.MaxRetryBackoff,
+		MinRetryBackoff: cfg.MinRetryBackoff,
+	}
+	return &Redis{Client: goredis.NewClusterClient(clusterOption)}
 }
